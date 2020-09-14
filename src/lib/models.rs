c99ba37870eea608a7ee8037c93feead40e4394c
ebina4yaka <4yaka3bina@gmail.com>
2020-09-14 12:57:33 +0900

    add color code in api response

--- src/lib/models.rs
+++ src/lib/models.rs
@@ -1,18 +1,20 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Error {
+pub struct ErrorMessage {
     pub message: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LanguagePercentage {
     pub name: String,
+    pub color: String,
     pub percentage: f64,
 }
 
 #[derive(Debug)]
 pub struct LanguageSize {
     pub name: String,
+    pub color: String,
     pub size: i64,
 }
